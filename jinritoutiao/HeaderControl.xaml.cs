54860f219acb6d8955b043768ffcaf861224a31b
gefangshuai <gefangshuai@163.com>
2014-07-03 13:58:59 +0800

    设计菜单

---
+++ jinritoutiao/HeaderControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// “用户控件”项模板在 http://go.microsoft.com/fwlink/?LinkId=234236 上提供
+
+namespace jinritoutiao
+{
+    public sealed partial class HeaderControl : UserControl
+    {
+        
+        public HeaderControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private void RefreshImage_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            RefreshImageStoryboard.Begin();
+        }
+
+        private void MenuListView_OnItemClick(object sender, ItemClickEventArgs e)
+        {
+            var item = sender as ListViewItem;
+            //Debug.WriteLine();
+        }
+    }
+}
--- jinritoutiao/MainPage.xaml.cs
+++ jinritoutiao/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -14,6 +15,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=391641 上有介绍
+using jinritoutiao.Common;
 
 namespace jinritoutiao
 {
@@ -22,27 +24,48 @@ namespace jinritoutiao
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private readonly NavigationHelper navigationHelper;
         public MainPage()
         {
             this.InitializeComponent();
-
+            StatusBar statusBar = StatusBar.GetForCurrentView();
+            statusBar.HideAsync();
             this.NavigationCacheMode = NavigationCacheMode.Required;
+
+            this.navigationHelper = new NavigationHelper(this);
+            navigationHelper.LoadState += navigationHelper_LoadState;
         }
+
+        void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+            MyHeaderControl.LoadStoryboard.Begin();
+        }
+
+        #region NavigationHelper 注册
 
         /// <summary>
-        /// 在此页将要在 Frame 中显示时进行调用。
+        /// 此部分中提供的方法只是用于使
+        /// NavigationHelper 可响应页面的导航方法。
+        /// <para>
+        /// 应将页面特有的逻辑放入用于
+        /// <see cref="NavigationHelper.LoadState"/>
+        /// 和 <see cref="NavigationHelper.SaveState"/> 的事件处理程序中。
+        /// 除了在会话期间保留的页面状态之外
+        /// LoadState 方法中还提供导航参数。
+        /// </para>
         /// </summary>
-        /// <param name="e">描述如何访问此页的事件数据。
-        /// 此参数通常用于配置页。</param>
+        /// <param name="e">提供导航方法数据和
+        /// 无法取消导航请求的事件处理程序。</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            // TODO: 准备此处显示的页面。
-
-            // TODO: 如果您的应用程序包含多个页面，请确保
-            // 通过注册以下事件来处理硬件“后退”按钮:
-            // Windows.Phone.UI.Input.HardwareButtons.BackPressed 事件。
-            // 如果使用由某些模板提供的 NavigationHelper，
-            // 则系统会为您处理该事件。
+            this.navigationHelper.OnNavigatedTo(e);
         }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            this.navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
     }
 }
